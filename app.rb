ad24e94dd0ab9ab58476c3b2b9a925a6c2ded032
Max Trubetskoy <handcrab@gmail.com>
2015-03-13 00:54:43 +0300

    add feature: show post

--- app.rb
+++ app.rb
@@ -11,6 +11,7 @@ configure do
   # set :mongo_db, conn.db('blog')
   # posts_collection = mongo_db.collection('posts')
   set :mongo_db, MongoClient.new.db('blog')
+  set :posts_collection, settings.mongo_db.collection('posts')
 end
 
 # model
@@ -22,10 +23,19 @@ class Post
   end
 
   def self.create(post)
-    # content_type :json
+    if post['permalink'].strip.empty?
+      post['permalink'] = post['title'].strip.gsub(/\s+/, '_')
+    end
+    # post['permalink'] ||= post['title'].strip.gsub(/\s+/, '_')
+    post['permalink'].downcase!
+    # raise if @collection.find_one permalink: post['permalink']
     new_id = @collection.insert post.merge(created_at: Time.now)
-    # id = object_id(id) if String === id
     @collection.find_one(_id: new_id)
+  end
+
+  def self.find_by_permalink(permalink)
+    # id = object_id(id) if String === id
+    @collection.find_one permalink: permalink
   end
 end
 
@@ -42,6 +52,13 @@ end
 post '/posts' do
   @post = Post.create params
   if @post
-    redirect '/'
-  end # TODO
+    redirect "/posts/#{@post['permalink']}"
+  else
+    redirect '/posts/new'
+  end
+end
+
+get '/posts/:permalink' do
+  @post = Post.find_by_permalink params[:permalink]
+  slim :show
 end
